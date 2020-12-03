381ad4af6847a569c50612254d2411962b60268e
Lucas C <61256306+serkeira@users.noreply.github.com>
2020-12-03 11:40:42 -0300

    projeto criado

---
+++ src/main/java/com/lucascerqueira/projeto/ProjetoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lucascerqueira.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+	}
+
+}
