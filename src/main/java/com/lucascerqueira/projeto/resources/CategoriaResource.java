47cac5b4a0d942e69e187c9bb30526012ddb0a04
Lucas C <61256306+serkeira@users.noreply.github.com>
2020-12-03 17:29:51 -0300

    primeira classe de dominio

--- src/main/java/com/lucascerqueira/projeto/resources/CategoriaResource.java
+++ src/main/java/com/lucascerqueira/projeto/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.lucascerqueira.projeto.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucascerqueira.projeto.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@GetMapping
-	public String listar() {
-		return "rest ok";
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
