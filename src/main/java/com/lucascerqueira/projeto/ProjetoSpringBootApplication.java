df32c2f73db51fc73d1823980e9cecf957c2b070
Lucas C <61256306+serkeira@users.noreply.github.com>
2020-12-04 10:55:54 -0300

    implementando CommandLineRunner e instanciacao

--- src/main/java/com/lucascerqueira/projeto/ProjetoSpringBootApplication.java
+++ src/main/java/com/lucascerqueira/projeto/ProjetoSpringBootApplication.java
@@ -1,13 +1,35 @@
 package com.lucascerqueira.projeto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSpringBootApplication {
+import com.lucascerqueira.projeto.domain.Categoria;
+import com.lucascerqueira.projeto.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+	
 }
