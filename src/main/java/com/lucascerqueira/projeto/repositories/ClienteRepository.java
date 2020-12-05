fab5809f479d5ee6cfb46fb1a33149735ccb546e
Lucas C <61256306+serkeira@users.noreply.github.com>
2020-12-05 11:31:15 -0300

    Cliente, logica enum TipoCliente, telefones, enderecos

---
+++ src/main/java/com/lucascerqueira/projeto/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.lucascerqueira.projeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucascerqueira.projeto.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
