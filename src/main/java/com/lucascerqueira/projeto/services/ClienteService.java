e32d7401091bd91c61c93b000bf8fccb3b5ca434
Lucas C <61256306+serkeira@users.noreply.github.com>
2020-12-10 10:54:34 -0300

    endpoint /clientes/{id}

---
+++ src/main/java/com/lucascerqueira/projeto/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.lucascerqueira.projeto.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucascerqueira.projeto.domain.Cliente;
+import com.lucascerqueira.projeto.repositories.ClienteRepository;
+import com.lucascerqueira.projeto.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+
+		Optional<Cliente> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+
+}
